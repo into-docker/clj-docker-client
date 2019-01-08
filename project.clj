05dee27fcc1b93aa07034257fc44c646fc7fdbed
Rahul De <rahul080327@gmail.com>
2019-01-08 17:23:27 +0100

    Update all headers to LGPL

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
 ;   This file is part of clj-docker-client.
 ;
 ;   clj-docker-client is free software: you can redistribute it and/or modify
-;   it under the terms of the GNU General Public License as published by
+;   it under the terms of the GNU Lesser General Public License as published by
 ;   the Free Software Foundation, either version 3 of the License, or
 ;   (at your option) any later version.
 ;
 ;   clj-docker-client is distributed in the hope that it will be useful,
 ;   but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;   GNU General Public License for more details.
+;   GNU Lesser General Public License for more details.
 ;
-;   You should have received a copy of the GNU General Public License
+;   You should have received a copy of the GNU Lesser General Public License
 ;   along with clj-docker-client. If not, see <http://www.gnu.org/licenses/>.
 
 (defproject lispyclouds/clj-docker-client "0.1.10"
   :author "Rahul De <rahul@mailbox.org>"
   :url "https://github.com/lispyclouds/clj-docker-client"
   :description "And idiomatic clojure client for Docker."
-  :license {:name "GPL 3.0"
+  :license {:name "LGPL 3.0"
             :url  "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [byte-streams "0.2.4"]
