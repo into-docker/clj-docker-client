b7a4fc2193ebb9bd5158dacb0fafcbc86e8e8752
Rahul De <rahul080327@gmail.com>
2019-03-03 10:21:56 +0100

    Fix desc typo

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
 (defproject lispyclouds/clj-docker-client "0.2.1"
   :author "Rahul De <rahul@mailbox.org>"
   :url "https://github.com/lispyclouds/clj-docker-client"
-  :description "And idiomatic clojure client for Docker."
+  :description "An idiomatic clojure client for Docker."
   :license {:name "LGPL 3.0"
             :url  "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
