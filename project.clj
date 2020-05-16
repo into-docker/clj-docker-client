ff77bfedbf547679b8d9f3a1084ce2410dac33ba
Rahul De <rahul080327@gmail.com>
2020-05-16 09:04:44 +0200

    bump deps

--- project.clj
+++ project.clj
@@ -21,15 +21,15 @@
             :url  "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-commons/clj-yaml "0.7.1"]
-                 [metosin/jsonista "0.2.5"]
+                 [metosin/jsonista "0.2.6"]
                  [unixsocket-http "1.0.5"]]
   :plugins [[lein-ancient "0.6.15"]]
   :global-vars {*warn-on-reflection* true}
-  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0-612"]]}
+  :profiles {:kaocha {:dependencies [[lambdaisland/kaocha "1.0.632"]]}
              :rebl   {:repl-options   {:nrepl-middleware [nrebl.middleware/wrap-nrebl]}
                       :injections     [(require '[cognitect.rebl :as rebl])]
                       :dependencies   [[rickmoynihan/nrebl.middleware "0.3.1"]
-                                       [org.clojure/core.async "1.1.587"]
+                                       [org.clojure/core.async "1.2.603"]
                                        [lein-cljfmt "0.6.7"]
                                        [org.openjfx/javafx-fxml "14.0.1"]
                                        [org.openjfx/javafx-controls "14.0.1"]
